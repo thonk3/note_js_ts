778b55ae6f4ddcfefaeb70681e991700c915a329
debubz <bao.es.hoang2@gmail.com>
2021-11-03 21:07:06 +1100

    what is this

--- learn/eloquent/05_high_order_func/wr_direction.js
+++ learn/eloquent/05_high_order_func/wr_direction.js
@@ -1,54 +1,65 @@
 const SCRIPTS = require("./scripts");
 
+/* 
+    SCRIPTS = [
+    {
+    name: string,
+    ranges: [[number, number]],
+    direction: string,
+    year: number,
+    living: boolean,
+    link: string
+    },
+    ]
+*/
+
 // something to help
 function charScript(code) {
-    for(let sc of SCRIPTS) {
-        if(sc.ranges.some(([from, to]) => {
-            return code >= from && code < to;
-        })) {
-            return sc;
-        }
+  for (let sc of SCRIPTS) {
+    if (
+      sc.ranges.some(([from, to]) => {
+        return code >= from && code < to;
+      })
+    ) {
+      return sc;
     }
+  }
 
-    return null;    // code not in anything
+  return null; // code not in anything
 }
 
 // something to help
 function countBy(items, groupName) {
-    let counts = [];
-    for(let item of items) {
-        let name = groupName(item);
-        let known = counts.findIndex(c => c.name == name);
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex((c) => c.name == name);
 
-        if(known == -1)
-            counts.push({ name, count: 1 });
-        else
-            counts[known].count++;
-    }
+    if (known == -1) counts.push({ name, count: 1 });
+    else counts[known].count++;
+  }
 
-    return counts;
+  return counts;
 }
 
 function domDir(text) {
-    let scripts = countBy(text, char => {
-        let scr = charScript(char.codePointAt(0));
+  let scripts = countBy(text, (char) => {
+    let scr = charScript(char.codePointAt(0));
 
-        return scr ? scr.name : "none";
-    }).filter(({name}) => name != "none");
+    return scr ? scr.name : "none";
+  }).filter(({ name }) => name != "none");
 
-    // if text count is 0 leave it
-    let total = scripts.reduce((n, {count}) => n+count, 0);
-    if(total === 0) return "None found";
+  // if text count is 0 leave it
+  let total = scripts.reduce((n, { count }) => n + count, 0);
+  if (total === 0) return "None found";
 
-    let dom = scripts.reduce((a, b) => a.count > b.count? a:b);
-    let dir = SCRIPTS.filter(sc => 
-        sc.name === dom.name )[0].direction;
+  let dom = scripts.reduce((a, b) => (a.count > b.count ? a : b));
+  let dir = SCRIPTS.filter((sc) => sc.name === dom.name)[0].direction;
 
-    return `${dir} - ${text}`;
+  return `${dir} - ${text}`;
 }
 
 console.log(domDir("Hello!"));
 // → ltr
 console.log(domDir("Hey, مساء الخير"));
 // → rtl
-
